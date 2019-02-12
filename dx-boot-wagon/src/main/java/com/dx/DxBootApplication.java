9c0b8f7ab53bc261442ba977b0150df9531220e8
姚俭 <yaoj@minstone.com.cn>
2019-02-12 17:39:48 +0800

    添加批处理命令

--- dx-boot-wagon/src/main/java/com/dx/DxBootApplication.java
+++ dx-boot-wagon/src/main/java/com/dx/DxBootApplication.java
@@ -7,6 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DxBootApplication  {
 
+    /**
+     * 命令：   mvn clean package wagon:upload-single wagon:sshexec
+     *
+     *
+     *
+     *
+     */
+
+
     public static void main(String[] args) {
         SpringApplication.run(DxBootApplication.class, args);
     }
