3ada64cd0b194f681748233c621a2ff1b0343ec9
steve <670139644@qq.com>
2019-03-09 18:03:55 +0800

    映入netty

---
+++ dx-boot-netty/src/main/java/com/dx/DxNettyBootApplication.java
@@ -0,0 +1,13 @@
+package com.dx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DxNettyBootApplication  {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DxNettyBootApplication.class, args);
+    }
+
+}
